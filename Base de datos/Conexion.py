d4660a96b55fe879b323f294f7dfc63d4d3510b5
Alvaro-1873 <alvaro.fernandez.payo@gmail.com>
2023-03-09 10:40:44 +0100

    subiendo

--- Base de datos/Conexion.py
+++ Base de datos/Conexion.py
@@ -1,16 +1,39 @@
-import pymysql
+import mariadb
+import sys
 
-conn = pymysql.connect(
-    host="localhost",
-    user="root",
-    password="toor",
-    database="employees"
-)
+try:
+    conn = mariadb.connect(
+        user="isaac",
+        password="1234",
+        host="172.26.0.11",
+        port=3306,
+        database="objetos"
+    )
+except mariadb.Error as e:
+    print(f"Error en la conexion a la base de datos de mariadb: {e}")
+    sys.exit(1)
 
-cursor = conn.cursor()
-cursor.execute("SELECT * FROM departments")
-result = cursor.fetchall()
-result2 = result[1]
-print(result2[1])
-cursor.close()
-conn.close()
+def consulta():
+    cur = conn.cursor()
+    nombre = "Tech X"
+    cur.execute("select id, nombre, descripcion, tipo, rareza, efecto from binding_of_isaac_objetos where nombre=?", (nombre,))
+    for id, nombre, descripcion, tipo, rareza, efecto in cur:
+        print(f"Id: {id}, Nombre: {nombre}, Descripcion: {descripcion}, Tipo: {tipo}, Rareza: {rareza}, Efecto: {efecto}")
+    conn.close()
+
+def insertar():
+    try:
+        cur = conn.cursor()
+        nombre="Magic Mushroom"
+        descripcion="Aumenta la salud, la lágrima, la velocidad y el daño del jugador"
+        tipo="Objeto pasivo"
+        rareza="Objeto especial"
+        efecto="El jugador aumenta la salud, la lágrima, la velocidad y el daño, lo que mejora su capacidad para sobrevivir y luchar"
+        imagen_url="https://bindingofisaacrebirth.fandom.com/wiki/Magic_Mushroom"
+        cur.execute("INSERT INTO binding_of_isaac_objetos (nombre,descripcion,tipo,rareza,efecto,imagen_url) VALUES (?, ?, ?, ?, ?, ?)", (nombre,descripcion,tipo,rareza,efecto,imagen_url))
+        conn.commit()
+        conn.close()
+    except mariadb.Error as e:
+        print(f"Error al insertar los datos: {e}")
+        sys.exit(1)
+    
